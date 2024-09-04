b0551fbe893ce5420a618d31cf27530833215408
DtHnAme <1904685725@qq.com>
2024-09-04 11:01:38 +0800

    add more info chips

--- src/renderer/src/components/connections/connection-item.tsx
+++ src/renderer/src/components/connections/connection-item.tsx
@@ -67,7 +67,24 @@ const ConnectionItem: React.FC<Props> = (props) => {
                 radius="sm"
                 variant="bordered"
               >
+                {[...info.chains].reverse()[0]}
+              </Chip>
+              <Chip
+                className="flag-emoji text-ellipsis whitespace-nowrap overflow-hidden"
+                size="sm"
+                radius="sm"
+                variant="bordered"
+              >
                 {info.chains[0]}
+              </Chip>
+              <Chip
+                className="flag-emoji text-ellipsis whitespace-nowrap overflow-hidden"
+                size="sm"
+                radius="sm"
+                variant="bordered"
+              >
+                {info.rule}
+                {info.rulePayload ? `(${info.rulePayload})` : ''}
               </Chip>
               <Chip size="sm" radius="sm" variant="bordered">
                 ↑ {calcTraffic(info.upload)} ↓ {calcTraffic(info.download)}
