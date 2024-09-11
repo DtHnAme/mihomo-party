37588a5de280aa49f1982f65f1bf19c4a6aff2f7
DtHnAme <1904685725@qq.com>
2024-09-11 13:09:54 +0800

    hidden second scroll

--- src/renderer/src/pages/connections.tsx
+++ src/renderer/src/pages/connections.tsx
@@ -159,13 +159,13 @@ const Connections: React.FC = () => {
         <ConnectionDetailModal onClose={() => setIsDetailModalOpen(false)} connection={selected} />
       )}
       <div className="overflow-x-auto sticky top-0 z-40">
-        <div className="flex p-2 gap-2 items-center">
+        <div className="flex p-2 gap-2">
           <Tabs 
             size="sm"
             color={`${tab === 'active' ? "primary" : "danger" }`}
             selectedKey={tab}
             variant="underlined"
-            className="w-fit"
+            className="w-fit h-[32px]"
             onSelectionChange={(key: Key) => {
               setTab(key as string)
             }}
@@ -212,7 +212,7 @@ const Connections: React.FC = () => {
 
           <Select
             size="sm"
-            className="w-[180px]"
+            className="w-[180px] min-w-[120px]"
             selectedKeys={new Set([sortBy])}
             onSelectionChange={async (v) => {
               setSortBy(v.currentKey as string)
